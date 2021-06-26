eb576314d9e73f28d8f88303f4f8fd470c847e10
Everton Dutra <evertondutra02@gmail.com>
2021-06-26 02:20:20 -0300

    adicionando a espera do driver

--- browser.py
+++ browser.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Browser:
     driver = webdriver.Chrome()
     driver.maximize_window()
+    wait = WebDriverWait(driver, 10)
+
